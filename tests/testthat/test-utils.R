8799db31a8e8d7a9e809863348ef44a248fd879b
Filip Schouwenaars <filip.schouwenaars@gmail.com>
2016-02-08 17:42:08 +0100

    add first tests

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,5 +1,8 @@
 context("utils")
 
 test_that("utility functions work as expected", {
-  expect_true(TRUE)
+  expect_equal(collapse(c("a")), "a")
+  expect_equal(collapse(c("a", "b")), "a and b")
+  expect_equal(collapse(c("a", "b", "c")), "a, b and c")
+  expect_equal(collapse(c("a", "b", "c"), conn = " or "), "a, b or c")
 })
