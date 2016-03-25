adf72cd42ba114919aeffa0074bfd63cd637431e
Filip Schouwenaars <filip.schouwenaars@gmail.com>
2016-03-25 18:31:39 +0100

    adapt settings, update gitignore

--- R/settings.R
+++ R/settings.R
@@ -1,3 +1,3 @@
 allowed_elements <- c("pre-exercise-code", "sample-code", "solution", "sct", "hint")
-required_elements <- c("sample-code", "solution", "sct")
+required_elements <- c("sample-code")
 project_alias <- "DataCamp Light"
