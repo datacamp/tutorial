2a52ee258c422643c6752057254a5e8ed91df651
Filip Schouwenaars <filip.schouwenaars@gmail.com>
2016-04-24 17:38:30 +0200

    register vignette builder in tutorial package

--- R/utils-vignettes.R
+++ R/utils-vignettes.R
@@ -7,7 +7,7 @@ register_vignette_engines <- function(pkg) {
 }
 
 vig_engine <- function(..., tangle = knitr:::vtangle) {
-  knitr:::vig_engine0(..., tangle = tangle, package = "rmarkdown", aspell = list(
+  knitr:::vig_engine0(..., tangle = tangle, package = "tutorial", aspell = list(
     filter = knitr:::knit_filter
   ))
 }
