892443a14ed69a93b6d99be76031426faecd8950
Jason Freels <Auburngrads@users.noreply.github.com>
2016-05-10 13:12:42 -0400

    Update utils-vignettes.R

--- R/utils-vignettes.R
+++ R/utils-vignettes.R
@@ -8,8 +8,7 @@ register_vignette_engines <- function(pkg) {
 
 #' @importFrom knitr knit_filter
 vig_engine <- function(..., tangle = knitr:::vtangle) {
-  knitr:::vig_engine(..., tangle = tangle, aspell = list(
-    filter = knit_filter
+  knitr:::vig_engine(..., tangle = tangle
   ))
 }
 
